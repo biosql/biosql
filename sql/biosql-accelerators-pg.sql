c9f015c4c777da08e856eef022d211749364e324
thomasd <thomasd@open-bio.org>
2002-03-01 09:57:51 +0000

    Updated accelerators for new schema...

--- sql/biosql-accelerators-pg.sql
+++ sql/biosql-accelerators-pg.sql
@@ -5,25 +5,22 @@ DROP FUNCTION biosql_accelerators_level ();
 
 CREATE FUNCTION biosql_accelerators_level () RETURNS int AS '
   BEGIN
-    RETURN 1;
+    RETURN 2;
   END;
 ' LANGUAGE 'plpgsql';
 
-DROP FUNCTION intern_seqfeature_key (text);
-
-CREATE FUNCTION intern_seqfeature_key (text) RETURNS int AS '
-  DECLARE
-    kname ALIAS FOR $1;
-    kid int;
+DROP FUNCTION intern_ontology_term (test) RETURNS int AS '
+  DECLATE
+    tname ALIAS FOR $1
   BEGIN
-    select into kid seqfeature_key_id from seqfeature_key where key_name = kname;
+    select into tid ontology_term_id from ontology_term where term_name = tname
     IF NOT FOUND THEN
-      INSERT INTO seqfeature_key (key_name) VALUES (kname);
-      RETURN currval(''seqfeature_ke_seqfeature_ke_seq'');
-    END IF;
-    RETURN kid;
-  END;
-' LANGUAGE 'plpgsql';
+      INSERT INTO ontology_term (term_name) VALUES (tname);
+      RETURN currval(''ontology_term_pkey_seq'');
+    END IF
+    RETURN tid;
+  END
+' LANGIAGE 'plpgsql';
 
 DROP FUNCTION intern_seqfeature_source (text);
 
@@ -35,27 +32,12 @@ CREATE FUNCTION intern_seqfeature_source (text) RETURNS int AS '
     select into kid seqfeature_source_id from seqfeature_source where source_name = kname;
     IF NOT FOUND THEN
       INSERT INTO seqfeature_source (source_name) VALUES (kname);
-      RETURN currval(''seqfeature_so_seqfeature_so_seq'');
+      RETURN currval(''seqfeature_source_pkey_seq'');
     END IF;
     RETURN kid;
   END;
 ' LANGUAGE 'plpgsql';
 
-DROP FUNCTION intern_seqfeature_qualifier (text);
-
-CREATE FUNCTION intern_seqfeature_qualifier (text) RETURNS int AS '
-  DECLARE
-    kname ALIAS FOR $1;
-    kid int;
-  BEGIN
-    select into kid seqfeature_qualifier_id from seqfeature_qualifier where qualifier_name = kname;
-    IF NOT FOUND THEN
-      INSERT INTO seqfeature_qualifier (qualifier_name) VALUES (kname);
-      RETURN currval(''seqfeature_qu_seqfeature_qu_seq'');
-    END IF;
-    RETURN kid;
-  END;
-' LANGUAGE 'plpgsql';
 
 DROP FUNCTION create_seqfeature (integer, text, text);
 
@@ -67,9 +49,9 @@ CREATE FUNCTION create_seqfeature (integer, text, text) RETURNS int AS '
   BEGIN
     INSERT INTO seqfeature 
            (bioentry_id, seqfeature_key_id, seqfeature_source_id)
-    VALUES (cs_bioentry_id, intern_seqfeature_key(cs_key), intern_seqfeature_source(cs_source));
+    VALUES (cs_bioentry_id, intern_ontology_term(cs_key), intern_seqfeature_source(cs_source));
     
-    RETURN currval(''seqfeature_seqfeature_id_seq'');
+    RETURN currval(''seqfeature_pkey_seq'');
   END;
 ' LANGUAGE 'plpgsql';
 
