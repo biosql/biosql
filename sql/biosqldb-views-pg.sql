968002984a60da0e9a2f9a97785aaf1949db854d
lapp <lapp@open-bio.org>
2008-02-22 01:12:19 +0000

    Adding LGPL license.

--- sql/biosqldb-views-pg.sql
+++ sql/biosqldb-views-pg.sql
@@ -1,4 +1,26 @@
---- THIS FILE IS EXPERIMENTAL; SOME OR ALL OF THESE COULD CHANGE
+-- $Id$
+--
+-- Copyright 2002-2003 Christopher Mungall
+-- Copyright 2008 Hilmar Lapp 
+-- 
+--  This file is part of BioSQL.
+--
+--  BioSQL is free software: you can redistribute it and/or modify it
+--  under the terms of the GNU Lesser General Public License as
+--  published by the Free Software Foundation, either version 3 of the
+--  License, or (at your option) any later version.
+--
+--  BioSQL is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Lesser General Public License for more details.
+--
+--  You should have received a copy of the GNU Lesser General Public License
+--  along with BioSQL. If not, see <http://www.gnu.org/licenses/>.
+--
+-- ========================================================================
+--
+-- THIS FILE IS EXPERIMENTAL; SOME OR ALL OF THESE COULD CHANGE
 
 CREATE VIEW seqfeature_key_v
  AS SELECT f.*, t.name AS key_name 
