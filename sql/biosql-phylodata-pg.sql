e20496eb466e6c9fd9636aedef37bf01875dbc73
lapp <lapp@open-bio.org>
2009-03-12 19:34:23 +0000

    Added cross-references for character states and characters. Added ontology term annotations for matrix cells, characters, and character states. Added foreign key constraints for those.

--- sql/biosql-phylodata-pg.sql
+++ sql/biosql-phylodata-pg.sql
@@ -120,7 +120,7 @@ COMMENT ON TABLE mchar IS 'A character in a character data matrix.';
 
 COMMENT ON COLUMN mchar.label IS 'The label of the character.';
 
--- qualifier/value (metadata) annotation for data characters
+-- qualifier/value (metadata) and ontology annotation for data characters
 CREATE TABLE mchar_qualifier_value (
        mchar_id INTEGER NOT NULL,       
        term_id INTEGER NOT NULL,
@@ -161,6 +161,27 @@ CREATE TABLE charstate (
 
 CREATE INDEX charstate_id ON charstate (label);
 
+-- qualifier/value (metadata) and ontology annotation for character states
+CREATE TABLE charstate_qualifier_value (
+       charstate_id INTEGER NOT NULL,
+       term_id INTEGER NOT NULL,
+       value TEXT,
+       rank INTEGER NOT NULL DEFAULT 0
+       , UNIQUE (charstate_id, term_id, rank)
+);
+
+CREATE INDEX charstate_qualifier_value_i1 ON charstate_qualifier_value (term_id);
+
+-- dbxrefs, such as identifiers, for character states of a data matrix
+CREATE TABLE charstate_dbxref (
+       charstate_id INTEGER NOT NULL,
+       dbxref_id INTEGER NOT NULL,
+       term_id INTEGER NOT NULL
+       , PRIMARY KEY (charstate_id, dbxref_id, term_id)
+);
+
+CREATE INDEX charstate_dbxref_i1 ON charstate_dbxref (dbxref_id);
+
 -- OTUs (rows of a character data matrix)
 CREATE SEQUENCE otu_pk_seq;
 CREATE TABLE otu (
@@ -173,11 +194,34 @@ COMMENT ON TABLE otu IS 'An OTU is an Operational Taxonomic Unit, the row in a p
 
 COMMENT ON COLUMN otu.label IS 'The label (or name) of an OTU.';
 
+-- qualifier/value (metadata) and ontology annotation for OTUs
+CREATE TABLE otu_qualifier_value (
+       otu_id INTEGER NOT NULL,
+       term_id INTEGER NOT NULL,
+       value TEXT,
+       rank INTEGER NOT NULL DEFAULT 0
+       , UNIQUE (otu_id, term_id, rank)
+);
+
+CREATE INDEX otu_qualifier_value_i1 ON otu_qualifier_value (term_id);
+
+-- dbxrefs, such as identifiers, for OTUs of a data matrix
+CREATE TABLE otu_dbxref (
+       otu_id INTEGER NOT NULL,
+       dbxref_id INTEGER NOT NULL,
+       term_id INTEGER NOT NULL
+       , PRIMARY KEY (otu_id, dbxref_id, term_id)
+);
+
+CREATE INDEX otu_dbxref_i1 ON otu_dbxref (dbxref_id);
+
+-- connecting tree nodes and OTUs
 CREATE TABLE node_otu (
        node_id INTEGER NOT NULL,
        otu_id INTEGER NOT NULL,
+       term_id INTEGER NOT NULL,
        rank INTEGER NOT NULL DEFAULT 0
-       , PRIMARY KEY (node_id, otu_id)
+       , PRIMARY KEY (node_id, otu_id, term_id)
 );
 
 CREATE INDEX node_otu_i1 ON node_otu (otu_id);
@@ -213,6 +257,7 @@ CREATE TABLE charmatrix_relationship (
 
 CREATE INDEX charmatrix_relationship_i1 ON charmatrix_relationship (child_charmatrix_id);
 
+-- data matrix cells (also sometimes called 'codings')
 CREATE SEQUENCE mcell_pk_seq;
 CREATE TABLE mcell (
        mcell_id INTEGER DEFAULT nextval('mcell_pk_seq') NOT NULL,
@@ -223,6 +268,10 @@ CREATE TABLE mcell (
        , CONSTRAINT mcell_c1 UNIQUE (mchar_id,otu_id,charmatrix_id)
 );
 
+CREATE INDEX mcell_i1 ON mcell (otu_id);
+CREATE INDEX mcell_i2 ON mcell (charmatrix_id);
+
+-- state values of a data matrix cell
 CREATE TABLE mcell_charstate (
        mcell_id INTEGER NOT NULL,
        charstate_id INTEGER NOT NULL,
@@ -231,6 +280,18 @@ CREATE TABLE mcell_charstate (
 );
 
 CREATE INDEX mcell_charstate_i1 ON mcell_charstate (charstate_id);
+
+-- qualifier/value (metadata) and ontology annotation for data matrix
+-- cells (codings)
+CREATE TABLE mcell_qualifier_value (
+       mcell_id INTEGER NOT NULL,
+       term_id INTEGER NOT NULL,
+       value TEXT,
+       rank INTEGER NOT NULL DEFAULT 0
+       , UNIQUE (mcell_id, term_id, rank)
+);
+
+CREATE INDEX mcell_qualifier_value_i1 ON mcell_qualifier_value (term_id);
 
 -- foreign keys and constraints:
 
@@ -297,6 +358,28 @@ ALTER TABLE charstate ADD CONSTRAINT FKmchar_charstate
       FOREIGN KEY (mchar_id) REFERENCES mchar (mchar_id)
       ON DELETE CASCADE;
 
+-- table charstate_qualifier_value:
+ALTER TABLE charstate_qualifier_value ADD CONSTRAINT FKcharstate_cstatequalvalue
+      FOREIGN KEY (charstate_id) REFERENCES charstate (charstate_id)
+      ON DELETE CASCADE;
+
+ALTER TABLE charstate_qualifier_value ADD CONSTRAINT FKterm_cstatequalvalue
+      FOREIGN KEY (term_id) REFERENCES term (term_id)
+      ON DELETE CASCADE;
+
+-- table charstate_dbxref
+ALTER TABLE charstate_dbxref ADD CONSTRAINT FKdbxref_cstatedbxref
+      FOREIGN KEY (dbxref_id) REFERENCES dbxref (dbxref_id)
+      ON DELETE CASCADE;
+
+ALTER TABLE charstate_dbxref ADD CONSTRAINT FKcharstate_cstatedbxref
+      FOREIGN KEY (charstate_id) REFERENCES charstate (charstate_id)
+      ON DELETE CASCADE;
+
+ALTER TABLE charstate_dbxref ADD CONSTRAINT FKterm_cstatedbxref
+      FOREIGN KEY (term_id) REFERENCES term (term_id)
+      ON DELETE CASCADE;
+
 -- table node_otu:
 ALTER TABLE node_otu ADD CONSTRAINT FKotu_nodeotu
       FOREIGN KEY (otu_id) REFERENCES otu (otu_id)
@@ -304,6 +387,10 @@ ALTER TABLE node_otu ADD CONSTRAINT FKotu_nodeotu
 
 ALTER TABLE node_otu ADD CONSTRAINT FKnode_nodeotu
       FOREIGN KEY (node_id) REFERENCES node (node_id)
+      ON DELETE CASCADE;
+
+ALTER TABLE node_otu ADD CONSTRAINT FKterm_nodeotu
+      FOREIGN KEY (term_id) REFERENCES term (term_id)
       ON DELETE CASCADE;
 
 -- table charmatrix_mchar:
@@ -356,4 +443,13 @@ ALTER TABLE mcell_charstate ADD CONSTRAINT FKmcell_mcell
 
 ALTER TABLE mcell_charstate ADD CONSTRAINT FKcharstate_mcell
       FOREIGN KEY (charstate_id) REFERENCES charstate (charstate_id)
+      ON DELETE CASCADE;
+
+-- table mcell_qualifier_value:
+ALTER TABLE mcell_qualifier_value ADD CONSTRAINT FKmcell_mcellqualvalue
+      FOREIGN KEY (mcell_id) REFERENCES mcell (mcell_id)
+      ON DELETE CASCADE;
+
+ALTER TABLE mcell_qualifier_value ADD CONSTRAINT FKterm_mcellqualvalue
+      FOREIGN KEY (term_id) REFERENCES term (term_id)
       ON DELETE CASCADE;
